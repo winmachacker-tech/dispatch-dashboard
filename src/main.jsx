5ebb6d40456815694ca78ad308497c5251eb7b6c
Mark Tishkun <winmachacker-tech@users.noreply.github.com>
2025-10-28 08:44:37 -0700

    Fix case-sensitive import: use ./App.jsx in main.jsx; enforce App.jsx filename

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from './App.jsx';
+import App from "./App.jsx";
 import "./index.css";
-import { initTheme } from './lib/theme';
-initTheme();
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
