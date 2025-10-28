e7691cc9dd0821c036265ec2e371cbade6243750
Mark Tishkun <winmachacker-tech@users.noreply.github.com>
2025-10-28 08:41:07 -0700

    Fix case-sensitive import and ensure App.jsx filename is tracked

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./app.jsx";
+import App from './App.jsx';
 import "./index.css";
 import { initTheme } from './lib/theme';
 initTheme();
